abaa5520e3c0ddb722afdb83667c43fe04c16c51
Gyan <gyanprakashdangi@gmail.com>
2024-07-18 09:43:52 +0530

    Day2: Graph-BFS Traversal

---
+++ Day2/BFS.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// vector<int> graphBFs(int node,vector<int>&visited, vector<int>adj[],vector<int>&ans){
+
+// 	visited[node]=1;
+// 	queue<int>q;
+// 	q.push(node);
+
+// 	while (!q.empty())
+// 	{
+// 		int front =q.front();
+// 		q.pop();
+// 		ans.push_back(front);
+
+// 		// sabi neighour ke liye:
+// 		for(auto i:adj[node]){
+// 			if(!visited[i]){
+// 				q.push(i);
+// 				visited[i]=1;
+// 			}
+// 		}
+// 	}
+// 	return ans;
+	
+// }
+
+
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+
+        
+        vector<int>ans;
+        queue<int>q;
+        q.push(0);
+        vector<int>visited(V,0);
+        
+        visited[0]=1;
+        
+        
+        while(!q.empty()){
+            int frontNode= q.front();
+            q.pop();
+            
+            ans.push_back(frontNode);
+            
+            for(auto i:adj[frontNode]){
+                
+                if(!visited[i]){
+                    q.push(i);
+                    visited[i]=1;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >>
+
+            E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // 		adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
